5acbd3f6893b0f3f45aeec06485fc3a5c766d3fb
surajyaligar2004 <surajyaligar18@gmail.com>
2025-03-24 15:28:32 +0530

    first

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig={
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:tailwindcss/recommended"
+    ],
+    plugins: ["prettier", "tailwindcss"],
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-escape-entities": "off",
+      "tailwindcss/classnames-order": "warn",
+      "tailwindcss/no-custom-classname": "warn",
+    },
+  })
+}
 
 export default eslintConfig;
