c9c8122f3092f07e51c330e8d680b31ae7be3b9a
surajyaligar2004 <surajyaligar18@gmail.com>
2025-03-30 21:35:09 +0530

    chabges

--- components/dashboard/credit-usage.tsx
+++ components/dashboard/credit-usage.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
+
 import { getPlanLimit, getPlanUsage } from "@/utils/plan-utils";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
