d296123ec0bdd2a564e91c849893891727069a0f
surajyaligar2004 <surajyaligar18@gmail.com>
2025-03-30 21:37:51 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['randomuser.me'],
   },
+  experimental: {
+    esmExternals: true,
+    // Enable better support for TypeScript path aliases
+    serverComponentsExternalPackages: []
+  },
 };
 
 module.exports = nextConfig; 
